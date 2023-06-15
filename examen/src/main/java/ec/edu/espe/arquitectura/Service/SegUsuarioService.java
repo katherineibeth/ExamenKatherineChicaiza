e9590fa31c4d69a7b2af8cd61fa4fb36cf667af8
katherineibeth <kichicaiza@espe.edu.ec>
2023-06-14 21:22:57 -0500

    Creacion de Servicio
    Katherine Chicaiza

---
+++ examen/src/main/java/ec/edu/espe/arquitectura/Service/SegUsuarioService.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.arquitectura.Service;
+
+import org.springframework.stereotype.Service;
+
+import ec.edu.espe.arquitectura.Repository.SegUsuarioRepository;
+
+@Service
+public class SegUsuarioService {
+
+    private final SegUsuarioRepository segUsuarioRepository;
+    
+}
